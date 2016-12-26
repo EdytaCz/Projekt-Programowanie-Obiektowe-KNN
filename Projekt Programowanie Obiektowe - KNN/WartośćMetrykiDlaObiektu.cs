fee633637a0b9caa9c6ab799f549ff7705decb3b
Edyta Czarnecka <czarnecka.ed@gmail.com>
2016-12-27 00:29:28 +0100

    -klasa WartośćMetrykiDlaObiektu.cs przechowująca wartości metryk

---
+++ Projekt Programowanie Obiektowe - KNN/WartośćMetrykiDlaObiektu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Programowanie_Obiektowe___KNN
+{
+    class WartośćMetrykiDlaObiektu
+    {
+        public WartośćMetrykiDlaObiektu(int indeks, double wartość, int klasa)
+        {
+            IndeksObiektu = indeks;
+            WartośćMetryki = wartość;
+            Klasa = klasa;
+        }
+        public int IndeksObiektu { get; set; }
+        public double WartośćMetryki { get; set; }
+        public int Klasa { get; set; }
+    }
+}
