9a0b90726a2f95ded5ede012800d6012c1e6bbe4
Edyta Czarnecka <czarnecka.ed@gmail.com>
2016-12-26 17:31:21 +0100

    -klasa Metryki.cs -dodanie metryki typu "Manhattan" do klasy Metryki.cs

---
+++ Projekt Programowanie Obiektowe - KNN/Metryki.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Programowanie_Obiektowe___KNN
+{
+    public class Metryki
+    {
+        public static double Manhattan(ObiektDecyzyjny treningowy, ObiektDecyzyjny testowy)
+        {
+            double suma = 0;
+            for (int i = 0; i < testowy.atrybuty.Count; i++)
+            {
+                suma += Math.Abs(Convert.ToDouble(testowy.atrybuty[i] - treningowy.atrybuty[i]));
+            }
+            return suma;
+        }
+    }
+}
