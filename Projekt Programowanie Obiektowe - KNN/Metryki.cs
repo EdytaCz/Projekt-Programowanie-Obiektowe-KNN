83a01409c9804c2fb29ea0914f1a1fb2e73145f1
Edyta Czarnecka <czarnecka.ed@gmail.com>
2016-12-27 09:28:04 +0100

    -dodane metryki "Canberr" oraz "Czebyszew"

--- Projekt Programowanie Obiektowe - KNN/Metryki.cs
+++ Projekt Programowanie Obiektowe - KNN/Metryki.cs
@@ -17,5 +17,38 @@ namespace Projekt_Programowanie_Obiektowe___KNN
             }
             return suma;
         }
+        public static double Euklides(ObiektDecyzyjny treningowy, ObiektDecyzyjny testowy)
+        {
+            double suma = 0;
+            for (int i = 0; i < testowy.atrybuty.Count; i++)
+            {
+                suma += Math.Pow(Convert.ToDouble(testowy.atrybuty[i] - treningowy.atrybuty[i]), 2);
+            }
+            return Math.Sqrt(suma);
+        }
+
+        public static double Canberr(ObiektDecyzyjny treningowy, ObiektDecyzyjny testowy)
+        {
+            double suma = 0;
+            for (int i = 0; i < testowy.atrybuty.Count; i++)
+            {
+                suma += Math.Abs(Convert.ToDouble((
+                    testowy.atrybuty[i] - treningowy.atrybuty[i]) / (testowy.atrybuty[i] + treningowy.atrybuty[i])));
+            }
+            return suma;
+        }
+
+        public static double Czebyszew(ObiektDecyzyjny treningowy, ObiektDecyzyjny testowy)
+        {
+            double suma = 0;
+            List<double> MaxWynik = new List<double>();
+
+            for (int i = 0; i < testowy.atrybuty.Count; i++)
+            {
+                suma = Math.Abs(Convert.ToDouble(testowy.atrybuty[i] - treningowy.atrybuty[i]));
+                MaxWynik.Add(suma);
+            }
+            return MaxWynik.Max();
+        }
     }
 }
